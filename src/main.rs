a519d8053799577caca95dc68315deb9d77a5ac2
Badel2 <2badel2@gmail.com>
2016-08-04 20:55:01 +0200

    Move code into shannon module, add some methods

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,81 @@
-// open.rs 
+// file open 
 use std::error::Error; 
-use std::fs::File; 
-use std::path::Path;
 use std::io::prelude::*; 
 // args
 use std::env;
 
-fn calculate_entropy<P: AsRef<Path>>(path : P) -> Result<f64,std::io::Error> {
-    let mut file = try!(File::open(path));
+mod shannon{
+use std;
+use std::fs::File; 
+use std::ffi::OsString;
+use std::io::prelude::*;
 
-    // This doesn't work for pipes
-    //let filesize : u64 = try!(fs::metadata(path)).len();
-    let mut filesize : u64 = 0;
-    let mut freq_table = [0u64; 256];
-    let mut buffer = [0; 1024];
+pub struct Shannon {
+    filename: OsString,
+    filesize: u64,
+    freq_table: [u64; 256],
+    entropy: f64,
+}
 
-    // Read x bytes using a buffer. At EOF, x == 0
-    loop {
-        let x = try!(file.read(&mut buffer));
-        if x == 0 { break; }
+impl Shannon {
+    pub fn open(path : &OsString) -> Result<Shannon,std::io::Error> {
+        //match shannon::Shannon::open(Path::new(&f),f.to_string_lossy().into_owned()){
+        let mut file = try!(File::open(path));
 
-        // Process x bytes:
-        for &byte in buffer.iter().take(x){
-            freq_table[byte as usize] += 1;
+        // This doesn't work for pipes
+        //let filesize : u64 = try!(fs::metadata(path)).len();
+        let mut filesize : u64 = 0;
+        let mut freq_table = [0u64; 256];
+        let mut buffer = [0; 1024];
+
+        // Read x bytes using a buffer. At EOF, x == 0
+        loop {
+            let x = try!(file.read(&mut buffer));
+            if x == 0 { break; }
+
+            // Process x bytes:
+            for &byte in buffer.iter().take(x){
+                freq_table[byte as usize] += 1;
+            }
+
+            filesize += x as u64;
         }
 
-        filesize += x as u64;
-    }
+        let mut entropy : f64 = 0.0;
 
-    let mut entropy : f64 = 0.0;
-
-    for &c in freq_table.iter(){
-        if c != 0 {
-            let temp : f64 = c as f64 / filesize as f64;
-            entropy += -temp * f64::log2(temp);
+        for &c in freq_table.iter(){
+            if c != 0 {
+                let temp : f64 = c as f64 / filesize as f64;
+                entropy += -temp * f64::log2(temp);
+            }
         }
-    }
 
-    Ok(entropy)
+        //let filename = String::from(path.to_string_lossy().into_owned());
+        let filename = path.clone();
+        Ok( Shannon { 
+            filename: filename,
+            filesize: filesize,
+            freq_table: freq_table,
+            entropy: entropy,
+        })
+    }
+    pub fn most_used_character(&self) -> u8 {
+        self.freq_table.iter().enumerate().map(|(x, y)| (y, x)).max().unwrap().1 as u8
+    }
+    pub fn filename(&self) -> String {
+        self.filename.to_string_lossy().into_owned()
+    }
+    pub fn filesize(&self) -> u64 {
+        self.filesize
+    }
+    pub fn freq_table(&self) -> [u64;256] {
+        self.freq_table
+    }
+    pub fn entropy(&self) -> f64 {
+        self.entropy
+    }
+}
+
 }
 
 fn main() { 
@@ -49,30 +87,31 @@ fn main() {
     }
 
     for f in args{
-        match calculate_entropy(&f){
+        match shannon::Shannon::open(&f){
             Err(why) => writeln!(&mut std::io::stderr(), 
                     "couldn't open {}: {}", f.to_string_lossy(), why.description())
                     .expect("failed printing to stderr"),
-            Ok(ent) => println!("{:.5}\t{}", ent, f.to_string_lossy()),
+            Ok(s) => println!("{:.5}\t{}", s.entropy(), s.filename()),
         }
     }
 }
 
 #[test]
 fn it_works(){
-    assert_eq!(calculate_entropy("test0").unwrap(), 0.0);
-    assert_eq!(calculate_entropy("test1").unwrap(), 0.0);
-    assert_eq!(calculate_entropy("test2").unwrap(), 7.982743005032543);
+    use shannon::Shannon;
+    assert_eq!(Shannon::open("test0").unwrap().entropy(), 0.0);
+    assert_eq!(Shannon::open("test1").unwrap().entropy(), 0.0);
+    assert_eq!(Shannon::open("test2").unwrap().entropy(), 7.982743005032543);
 }
 
 #[test]
 #[should_panic]
 fn it_panics(){
-    let _ent = calculate_entropy("").unwrap();
+    let _ent = Shannon::open("").unwrap();
 }
 
 #[test]
 #[should_panic]
 fn it_panics_also(){
-    let _ent = calculate_entropy("filethatdoenstexist").unwrap();
+    let _ent = Shannon::open("filethatdoenstexist").unwrap();
 }
